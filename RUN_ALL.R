70235443aedfdfcec6a45b4563f687c26901397e
Quinn White <qwhite@smith.edu>
2023-02-21 07:55:04 -0500

    add script to run all rmarkdown files

---
+++ RUN_ALL.R
@@ -0,0 +1,9 @@
+
+
+library(rmarkdown)
+
+render("./load_wrangle_filter_data.Rmd", output_dir = "./output_html" )
+render("./generate_table.Rmd", output_dir = "./output_html")
+render("./filter_out_filings.Rmd",output_dir = "./output_html")
+render("./retrieve_endowment.Rmd", output_dir = "./output_html")
+render("./data_dictionary.Rmd", output_dir = "./output_html")
