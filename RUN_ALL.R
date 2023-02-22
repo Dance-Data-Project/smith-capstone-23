d17b7d1291a435379967e27f23352c2aa90c9de5
Andrew Hoekstra <andrew@pointevector.com>
2023-02-22 16:16:43 -0500

    Print out environment variables to see if that's the pipeline issue.

--- RUN_ALL.R
+++ RUN_ALL.R
@@ -4,14 +4,14 @@
 # in the output_html directory
 ##################################################################
 
-library(rmarkdown)
-
 if (file.exists(".env")) {
   dotenv::load_dot_env()
 }
 
-render("./load_wrangle_filter_data.Rmd", output_dir = "./output_html" )
-# render("./generate_table.Rmd", output_dir = "./output_html")
-# render("./filter_out_filings.Rmd",output_dir = "./output_html")
-# render("./retrieve_endowment.Rmd", output_dir = "./output_html")
-# render("./data_dictionary.Rmd", output_dir = "./output_html")
+print(Sys.getenv())
+
+# rmarkdown::render("load_wrangle_filter_data.Rmd", output_dir = "./output_html")
+# rmarkdown::render("generate_table.Rmd", output_dir = "./output_html")
+# rmarkdown::render("filter_out_filings.Rmd", output_dir = "./output_html")
+# rmarkdown::render("retrieve_endowment.Rmd", output_dir = "./output_html")
+# rmarkdown::render("data_dictionary.Rmd", output_dir = "./output_html")
