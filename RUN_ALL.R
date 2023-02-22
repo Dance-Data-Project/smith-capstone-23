3055136a36ec4449686d14a62b44cda34daca09d
Andrew Hoekstra <andrew@pointevector.com>
2023-02-22 16:36:53 -0500

    Explicitly set environment variables from pipeline variables.

--- RUN_ALL.R
+++ RUN_ALL.R
@@ -8,9 +8,7 @@ if (file.exists(".env")) {
   dotenv::load_dot_env()
 }
 
-print(Sys.getenv())
-
-# rmarkdown::render("load_wrangle_filter_data.Rmd", output_dir = "./output_html")
+rmarkdown::render("load_wrangle_filter_data.Rmd", output_dir = "./output_html")
 # rmarkdown::render("generate_table.Rmd", output_dir = "./output_html")
 # rmarkdown::render("filter_out_filings.Rmd", output_dir = "./output_html")
 # rmarkdown::render("retrieve_endowment.Rmd", output_dir = "./output_html")
