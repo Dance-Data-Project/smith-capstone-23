56668a6ec47ee5c4b58e26d91dee1869e2e229a6
Quinn White <qwhite@smith.edu>
2023-03-21 12:09:50 -0400

    specify ordering in which infrastructure files are run, and run infrastructure files before the exploration files

--- RUN_ALL.R
+++ RUN_ALL.R
@@ -7,6 +7,32 @@
 library(rmarkdown)
 library(here)
 
+
+# Infrastructure folder 
+files <- tibble(full = dir(here("infrastructure_rmds"),
+                           full.names = TRUE),
+             partial = dir(here("infrastructure_rmds")))
+             
+output <- here("infrastructure_rmds", "output_html")
+
+# order files to account for dependencies
+# filter_out_filings depends on output of load_wrangle_filter
+files_ordered <- tibble(partial = c(
+  "load_wrangle_filter_data.Rmd",
+           "loading_error_test.Rmd",
+           "filter_out_filings.Rmd",
+           "howto_get_vars.Rmd",
+           "data_dictionary.Rmd")) %>%
+  left_join(files)
+
+files <- files_ordered$full
+  
+for (i in files) {
+  print(i)
+  if(grepl(".Rmd", i)){render(i, output_dir = output)}
+}
+
+
 # exploration folder
 files <- dir(here("explorations_rmds"),
              full.names = TRUE)
@@ -16,12 +42,3 @@ for (i in files) {
   if(grepl(".Rmd", i)) { render(i, output_dir = output)}
 }
 
-# Infrastructure folder 
-files <- dir(here("infrastructure_rmds"),
-             full.names = TRUE)
-output <- here("infrastructure_rmds", "output_html")
-
-for (i in files) {
-  print(i)
-  if(grepl(".Rmd", i)){render(i, output_dir = output)}
-}
